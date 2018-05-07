ee4d0c70df9d03e09d20f6c7a111ac4e5f2b88f1
hfukuda <hajime.fukuda@me.com>
2018-05-07 23:52:14 +0900

    changed to 2018

--- js/main.js
+++ js/main.js
@@ -1,24 +1,3 @@
-$(() => {
-  let talksData = JSON.parse(data.replace("\n", ""));
-  let majorsData = JSON.parse(major);
+$(function () {
 
-  let rikei = [];
-  let bunkei = []
-
-  for (let i = 0; i < talksData.length; i++) {
-    let el = talksData[i];
-    el.major = majorsData[i].major;
-    if (majorsData[i].bunri == 0) {
-      rikei.push(el);
-    } else {
-      bunkei.push(el);
-    }
-  }
-
-
-  let template = $('#template').html();
-  Mustache.parse(template);
-  let renderedRikei = Mustache.render(template, {talks: rikei, header: "理工系講演"});
-  let renderedBunkei = Mustache.render(template, {talks: bunkei, header: "人文系講演"});
-  $('.article-headline').html(renderedRikei + renderedBunkei);
 });
--- js/timetable.js
+++ js/timetable.js
@@ -1,24 +1,3 @@
-function construstTimeTable(timeTable, talksData, majorsData) {
-  return timeTable.map(d => {
-    let talk = talksData.filter(talk => talk.name.startsWith(d.name))[0];
-    let major = majorsData.filter(major => major.name.startsWith(d.name))[0];
-    return {time: d.time, name: talk.name, title: talk.title, major: major.major};
-  });
-}
+$(function () {
 
-
-$(() => {
-  let talksData = JSON.parse(data.replace("\n", ""));
-  let majorsData = JSON.parse(major);
-  let firstTable = JSON.parse(firstDay);
-  let secondTable = JSON.parse(secondDay);
-
-  let firstDayTable = construstTimeTable(firstTable, talksData, majorsData);
-  let secondDayTable = construstTimeTable(secondTable, talksData, majorsData);
-
-  let template = $('#template').html();
-  Mustache.parse(template);
-  let renderedFirst = Mustache.render(template, {table: firstDayTable, header: "5/20(土)"});
-  let renderedSecond = Mustache.render(template, {table: secondDayTable, header: "5/21(日)"});
-  $('.article-headline').html(renderedFirst + "<br />" + renderedSecond);
 });
